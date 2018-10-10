01387ef8684d4704dbc9c93ff920d476342ee74a
Yasmin <ym222bs@student.lnu.se>
2018-10-10 19:55:47 +0200

    no

---
+++ database/new_config.php
@@ -0,0 +1,11 @@
+<?php
+// Using define();
+/*              A constant's value cannot be changed after it is set
+                Constant names do not need a leading dollar sign ($)
+IMPORTANT ->    Constants can be accessed regardless of scope
+                Constant values can only be strings and numbers
+*/
+                define('DB_HOST', 'localhost');
+                define('DB_USER', 'root');
+                define('DB_PASSWORD', '');
+                define('DB_NAME', 'mysqldb');
